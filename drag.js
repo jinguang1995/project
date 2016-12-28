463f708f01f95dc7430646786493f8c635606684
jinguang1995 <jinguang1995@sohu.com>
2016-12-28 21:23:42 +0800

    change drag

--- drag.js
+++ drag.js
@@ -2,7 +2,10 @@
 * @Author: anchen
 * @Date:   2016-12-28 20:21:53
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-12-28 20:57:39
+* @Last Modified time: 2016-12-28 21:23:00
 */
 
 'use strict';
+function drag(){
+
+};
