971bc1c88941b73fb206bd3b16114f7d943c7b7d
jinguang1995 <jinguang1995@sohu.com>
2016-12-28 21:42:01 +0800

    change3 drag.js

--- drag.js
+++ drag.js
@@ -2,12 +2,14 @@
 * @Author: anchen
 * @Date:   2016-12-28 20:21:53
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-12-28 21:25:16
+* @Last Modified time: 2016-12-28 21:41:31
 */
 
 'use strict';
 function drag(id){
     var obj = document.getElementById(id);
+    var disX=0;
+    var disY=0;
     obj.onmousedown=function(){
         
     }
