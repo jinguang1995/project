b2e95ea7c196478b5e492d0f34e4f2f2ffa5859e
jinguang1995 <jinguang1995@sohu.com>
2016-12-28 21:31:47 +0800

    change2

--- drag.js
+++ drag.js
@@ -2,10 +2,13 @@
 * @Author: anchen
 * @Date:   2016-12-28 20:21:53
 * @Last Modified by:   anchen
-* @Last Modified time: 2016-12-28 21:23:00
+* @Last Modified time: 2016-12-28 21:25:16
 */
 
 'use strict';
-function drag(){
-
+function drag(id){
+    var obj = document.getElementById(id);
+    obj.onmousedown=function(){
+        
+    }
 };
